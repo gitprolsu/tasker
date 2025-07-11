9dbdcfed7d9e08c521ad6b64448e7d9a7044acd3
gitprolsu <96758418+gitprolsu@users.noreply.github.com>
2025-07-11 12:35:50 -0400

    Update taskerlogic.js

--- taskerlogic.js
+++ taskerlogic.js
@@ -19,145 +19,76 @@ postArea.innerHTML = `
     </form>
     `;
 
-function retrieveTasks(taskIndex) {
-  let status,
-    color,
-    timeStat,
-    timeStatColor,
-    taskTimeStamp,
-    foundTask,
-    taskId = taskIndex;
-  foundTask = JSON.parse(localStorage.taskerObject);
-  foundTask = foundTask[taskId];
-
-  if (foundTask.updatedTime === "") {
-    timeStat = "Posted";
-    timeStatColor = "tomato";
-    taskTimeStamp = foundTask.timeStamp;
-  } else {
-    timeStat = "Updated";
-    timeStatColor = "rgb(0, 113, 365)";
-    taskTimeStamp = foundTask.updatedTime;
+function initialAppSettings() {
+  let taskerSettings, appSettings, currentScreen;
+  if (localStorage.taskerSettings == undefined) {
+    taskerSettings = JSON.parse(localStorage.taskerSettings);
+    appSettings = {
+      version: 1.01,
+      screen: "Light",
+    };
+    currentScreen = "lightMode";
+    localStorage.setItem("taskerSettings", JSON.stringify(appSettings));
   }
 
-  if (foundTask.completed === true) {
-    status = "Done";
-    color = "green";
-    timeStat = "Completed";
-    timeStatColor = "green";
-    taskTimeStamp = foundTask.timeCompleted;
-  } else if (foundTask.completed === false) {
-    status = "Active";
-    color = "tomato";
-    let divElement = document
-      .createElement("div")
-      .setAttribute("id", foundTask.taskId);
-
-    divElement.innerHTML = `
-                    <div class="col-12 col-sm-12">
-                        <div class="row">
-                            <div class="col-12 col-sm-12">
-                                <h4 type="button" class="postedTask">${foundTask.taskTitle}</h4>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 col-sm-12 taskDescription">
-                                <p>${foundTask.taskDescription}</p>
-                            </div>
-                            <div class="col-3 col-sm-4 taskStatus">
-                                <h6>Status: <span style="color: ${color};">${status}</span></h6>
-                            </div>
-                            <div class="col-9 col-sm-8 taskTimeStamp">
-                                <h6>${timeStat}: <span style="color: ${timeStatColor};">${taskTimeStamp}</span></h6>
-                            </div>
-                        </div>
-                    </div>
-                    <hr/>
-                    `;
-    document.getElementById("taskContainer").prepend(divElement);
-  }
+  document.querySelector("body").setAttribute("class", currentScreen);
 }
 
-function retrieveActiveTasks() {
-  let storedTasks = [];
-  let allContainers;
-  let taskerObject = localStorage.taskerObject;
+function switchScreenMode() {
+  let settings = JSON.parse(localStorage.taskerSettings);
 
-  if (taskerObject !== undefined) {
-    taskerObject = JSON.parse(taskerObject);
-    taskerObject = Object.keys(taskerObject);
-
-    for (var i = 0; i < taskerObject.length; i++) {
-      storedTasks.push(taskerObject[i]);
-    }
+  if (settings.screen === "Dark") {
+    document.querySelector("body").setAttribute("class", "lightMode");
+    settings.screen = "Light";
+  } else if (settings.screen === "Light") {
+    document.querySelector("body").setAttribute("class", "nightOwl");
+    settings.screen = "Dark";
   }
+  localStorage.setItem("taskerSettings", JSON.stringify(settings));
+  window.location.reload();
+}
 
-  if (storedTasks) {
-    if (storedTasks) storedTasks.forEach((task) => retrieveTasks(task));
-  } else if (taskerObject == undefined) {
-    localStorage.setItem("taskerObject", "");
+function settings() {
+  console.log("hello settings!");
 
-    let emptyElement = document.createElement("div");
-    emptyElement.setAttribute("class", "row");
+  let currentScreenMode =
+    localStorage.taskerSettings.screen == "Light" ? "Light" : "Dark";
 
-    emptyElement.innerHTML = `
-            <div class="col-0 col-sm-1"></div>
-            <div class="col-12 col-sm-11" id="emptyContainer">
-                <h6>No tasks yet! Enter your first task above and press enter or click on 'post'</h6>
+  console.log(currentScreenMode);
+
+  let settingsElement = document
+    .createElement("div")
+    .setAttribute("class", "col-12 col-sm-12");
+
+  settingsElement.innerHTML = `
+        <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+                <div class="row">
+                    <div class="col-3 col-sm-3">
+                        <p id="darkModeLabel">Screen Mode: <img type="button" onclick="switchScreenMode()" id="darkModeBtn" src="icons/dark-mode.png" title="Screen Mode"/> ${currentScreenMode}</p>
+                    </div>
+                    <div class="col-3 col-sm-3">
+                        <p id="deleteStorage">Delete all tasks: <img type="button" onclick="factoryReset()" id="resetBtn" src="icons/reset.png" title="Reset settings and delete stored tasks" />                                
+                    </div>
+                </div>
             </div>
-        `;
-
-    allContainers = document
-      .querySelector("#taskContainer")
-      .prepend(emptyElement);
-  }
-
-  let activeTasks = document.getElementsByClassName("postedTask");
-
-  for (var i = 0; i < activeTasks.length; i++) {
-    activeTasks[i].parentElement.addEventListener("click", function (data) {
-      showUpdateTasker(data);
-    });
-  }
+            <div class="col-sm-2"></div>
+        </div>
+        <hr />
+    `;
+  document.getElementById("settingsArea").append(settingsElement);
 }
 
-function retrieveCompletedTasks() {
-  let storedtaskIds,
-    taskerObject = localStorage.taskerObject;
-  taskerObject = JSON.parse(taskerObject);
-  taskerObject = Object.keys(taskerObject);
-  storedtaskIds = [];
+function factoryReset() {
+  let confirmed = confirm(
+    "Are you sure you want to delete all your tasks from this computer?"
+  );
 
-  for (var i = 0; i < taskerObject.length; i++) {
-    storedtaskIds.push(taskerObject[i]);
-  }
-
-  if (storedtaskIds)
-    storedtaskIds.forEach((taskId) => retrieveDoneTasks(taskId));
-
-  function retrieveDoneTasks(taskArg) {
-    let foundTask,
-      index = taskArg;
-    foundTask = JSON.parse(localStorage.taskerObject);
-
-    if (foundTask[index].completed) {
-      let divElement = document.createElement("div");
-      divElement.setAttribute("id", `${foundTask[index].taskId}done`);
-
-      divElement.innerHTML = `
-                <div class="row">
-                    <div class="col-12 col-sm-12">
-                        <h4 type="button" class="postedTask">${foundTask[index].taskTitle}</h4>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-sm-12 taskTimeStamp">
-                        <h6>Completed on: <span style="color: green;">${foundTask[index].timeCompleted}</span></h6>
-                    </div>
-                </div>
-            <hr/>`;
-      document.getElementById("tasksCompleted").append(divElement);
-    }
+  if (confirmed) {
+    localStorage.removeItem("taskerObject");
+    localStorage.removeItem("taskerSettings");
+    window.location.reload();
   }
 }
 
@@ -200,6 +131,131 @@ function createNewTask() {
   window.location.reload();
 }
 
+function pullTasksFromLocalStorage() {
+  let listKeys,
+    taskList = JSON.parse(localStorage.taskerObject);
+  let storedTasks = [];
+  let allContainers = null;
+
+  if (taskList !== undefined) {
+    listKeys = Object.keys(taskList);
+
+    for (var i = 0; i < taskerObject.length; i++) {
+      storedTasks.push(taskerObject[i]);
+    }
+    storedTasks.forEach((task) => displayTasksToScreen(task));
+  } else {
+    localStorage.setItem("taskerObject", "");
+
+    let emptyElement = document
+      .createElement("div")
+      .setAttribute("class", "row");
+
+    emptyElement.innerHTML = `
+            <div class="col-0 col-sm-1"></div>
+            <div class="col-12 col-sm-11" id="emptyContainer">
+                <h6>No tasks yet! Enter your first task above and press enter or click on 'post'</h6>
+            </div>
+        `;
+
+    allContainers = document
+      .querySelector("#taskContainer")
+      .prepend(emptyElement);
+  }
+}
+
+function displayTasksToScreen(taskIndex) {
+  let tasks = JSON.parse(localStorage.taskerObject);
+  let currentTask = tasks[taskIndex];
+
+  let status, color, timeStat, timeStatColor, taskTimeStamp;
+
+  if (currentTask.updatedTime === "") {
+    timeStat = "Posted";
+    timeStatColor = "tomato";
+    taskTimeStamp = currentTask.timeStamp;
+  } else {
+    timeStat = "Updated";
+    timeStatColor = "rgb(0, 113, 365)";
+    taskTimeStamp = currentTask.updatedTime;
+  }
+
+  if (currentTask.completed === true) {
+    status = "Done";
+    color = "green";
+    timeStat = "Completed";
+    timeStatColor = "green";
+    taskTimeStamp = currentTask.timeCompleted;
+  } else if (currentTask.completed === false) {
+    status = "Active";
+    color = "tomato";
+    let divElement = document
+      .createElement("div")
+      .setAttribute("id", currentTask.taskId);
+
+    divElement.innerHTML = `
+                    <div class="col-12 col-sm-12">
+                        <div class="row">
+                            <div class="col-12 col-sm-12">
+                                <h4 type="button" class="postedTask" onclick="showUpdateTasker('${currentTask.id}')">${currentTask.taskTitle}</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-12 taskDescription">
+                                <p>${currentTask.taskDescription}</p>
+                            </div>
+                            <div class="col-3 col-sm-4 taskStatus">
+                                <h6>Status: <span style="color: ${color};">${status}</span></h6>
+                            </div>
+                            <div class="col-9 col-sm-8 taskTimeStamp">
+                                <h6>${timeStat}: <span style="color: ${timeStatColor};">${taskTimeStamp}</span></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                    `;
+    document.getElementById("taskContainer").prepend(divElement);
+  }
+}
+
+function displasyCompletedTasks() {
+  let taskerObject = localStorage.taskerObject;
+  if (taskerObject !== undefined) {
+    taskerObject = JSON.parse(taskerObject);
+    taskerObject = Object.keys(taskerObject);
+    let storedtaskIds = [];
+
+    for (var i = 0; i < taskerObject.length; i++) {
+      storedtaskIds.push(taskerObject[i]);
+    }
+    storedtaskIds.forEach((taskId) => retrieveDoneTasks(taskId));
+  }
+}
+
+function retrieveDoneTasks(taskArg) {
+    let foundTask = JSON.parse(localStorage.taskerObject);
+    let index = taskArg;
+
+    if (foundTask[index].completed) {
+      let divElement = document.createElement("div");
+      divElement.setAttribute("id", `${foundTask[index].taskId}done`);
+
+      divElement.innerHTML = `
+                <div class="row">
+                    <div class="col-12 col-sm-12">
+                        <h4 type="button" class="postedTask">${foundTask[index].taskTitle}</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-12 taskTimeStamp">
+                        <h6>Completed on: <span style="color: green;">${foundTask[index].timeCompleted}</span></h6>
+                    </div>
+                </div>
+            <hr/>`;
+      document.getElementById("tasksCompleted").append(divElement);
+    }
+  }
+
 function showUpdateTasker(data) {
   let taskId = data.path[4].id;
 
@@ -234,134 +290,57 @@ function showUpdateTasker(data) {
 function postUpdate(dataId) {
   // console.log(`Update post with id: ${dataId}`);
 
-  let currentLocalStorage = JSON.parse(localStorage.taskerObject);
+  let tasks = JSON.parse(localStorage.taskerObject);
 
   let updatedTitle = document.getElementById("taskToUpdate").value;
   let updatedDescription = document.getElementById("descriptionToUpdate").value;
 
-  if (!updatedTitle) {
-    window.location.reload();
+  if (updatedTitle == "") {
+    alert("Title require to update post!");
   } else {
-    currentLocalStorage[dataId].taskTitle = updatedTitle;
-    currentLocalStorage[dataId].taskDescription = updatedDescription;
+    let comfirmed =
+      updatedDescription == "" ? confirm("Continue without description?") : "";
+
+    if (comfirmed) {
+    }
+    tasks[dataId].taskTitle = updatedTitle;
+    tasks[dataId].taskDescription = updatedDescription;
+    let newDateUpdate = new Date().toString().split(" ");
     let newTimeStamp = "";
-    let newDateUpdate = new Date();
-    newDateUpdate = newDateUpdate.toString().split(" ");
 
     for (var i = 0; i < 5; i++) {
       newTimeStamp += `${newDateUpdate[i]} `;
     }
 
-    currentLocalStorage[dataId].updatedTime = newTimeStamp;
+    tasks[dataId].updatedTime = newTimeStamp;
 
-    localStorage.setItem("taskerObject", JSON.stringify(currentLocalStorage));
+    localStorage.setItem("taskerObject", JSON.stringify(tasks));
     window.location.reload();
   }
 }
 
 function deletePost(dataId) {
   // console.log(`Delete post with id: ${dataId}`);
-  let currentStorage = JSON.parse(localStorage.taskerObject);
+  let tasks = JSON.parse(localStorage.taskerObject);
 
-  delete currentStorage[dataId];
-  localStorage.setItem("taskerObject", JSON.stringify(currentStorage));
+  delete tasks[dataId];
+  localStorage.setItem("taskerObject", JSON.stringify(tasks));
   window.location.reload();
 }
 
 function taskDone(dataId) {
   // console.log(`Task with id ${dataId} completed. Yay!`);
-  let currentLocalStorage = JSON.parse(localStorage.taskerObject);
-  currentLocalStorage[dataId].completed = true;
+  let tasks = JSON.parse(localStorage.taskerObject);
+  tasks[dataId].completed = true;
   let completeTimeStamp = "";
-  let completedDate = new Date();
-  completedDate = completedDate.toString().split(" ");
+  let completedDate = new Date().toString().split(" ");
   for (var i = 0; i < 5; i++) {
     completeTimeStamp += `${completedDate[i]} `;
   }
 
-  currentLocalStorage[dataId].timeCompleted = completeTimeStamp;
-  localStorage.setItem("taskerObject", JSON.stringify(currentLocalStorage));
+  tasks[dataId].timeCompleted = completeTimeStamp;
+  localStorage.setItem("taskerObject", JSON.stringify(tasks));
   window.location.reload();
-}
-
-function initialAppSettings() {
-  let appSettings, currentScreen, taskerSettings;
-  if (localStorage.taskerSettings == undefined) {
-    appSettings = {
-      version: 1.01,
-      screen: "Light",
-    };
-    currentScreen = "lightMode";
-    localStorage.setItem("taskerSettings", JSON.stringify(appSettings));
-  }
-
-  document.querySelector("body").setAttribute("class", currentScreen);
-}
-
-function settings() {
-  console.log("hello settings!");
-
-  let currentScreenMode,
-    taskerSettings = localStorage.taskerSettings;
-  if (taskerSettings) {
-    taskerSettings = JSON.parse(taskerSettings);
-
-    if (taskerSettings.screen === "Dark") {
-      currentScreenMode = "Dark";
-    } else if (taskerSettings.screen === "Light") {
-      currentScreenMode = "Light";
-    }
-  }
-
-  console.log(currentScreenMode);
-
-  let settingsElement = document.createElement("div");
-  settingsElement.setAttribute("class", "col-12 col-sm-12");
-
-  settingsElement.innerHTML = `
-        <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-8">
-                <div class="row">
-                    <div class="col-3 col-sm-3">
-                        <p id="darkModeLabel">Screen Mode: <img type="button" onclick="switchScreenMode()" id="darkModeBtn" src="icons/dark-mode.png" title="Screen Mode"/> ${currentScreenMode}</p>
-                    </div>
-                    <div class="col-3 col-sm-3">
-                        <p id="deleteStorage">Delete all tasks: <img type="button" onclick="factoryReset()" id="resetBtn" src="icons/reset.png" title="Reset settings and delete stored tasks" />                                
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-2"></div>
-        </div>
-        <hr />
-    `;
-  document.getElementById("settingsArea").append(settingsElement);
-}
-
-function switchScreenMode() {
-  let settings = JSON.parse(localStorage.taskerSettings);
-
-  if (settings.screen === "Dark") {
-    document.querySelector("body").setAttribute("class", "lightMode");
-    settings.screen = "Light";
-  } else if (settings.screen === "Light") {
-    document.querySelector("body").setAttribute("class", "nightOwl");
-    settings.screen = "Dark";
-  }
-  localStorage.setItem("taskerSettings", JSON.stringify(settings));
-  window.location.reload();
-}
-
-function factoryReset() {
-  let confirmed = confirm(
-    "Are you sure you want to delete all your tasks from this computer?"
-  );
-
-  if (confirmed) {
-    localStorage.removeItem("taskerObject");
-    localStorage.removeItem("taskerSettings");
-    window.location.reload();
-  }
 }
 
 // create new tasks
@@ -373,7 +352,9 @@ document.getElementById("newBtn").addEventListener("click", function (event) {
 // adjust page settings
 document.getElementById("settingsBtn").addEventListener("click", function () {
   let settingsUp = document.getElementById("settingsArea").children[0];
-  if (!settingsUp) settings();
+  if (!settingsUp) {
+    settings();
+  }
 });
 
 // display optional input box:
@@ -388,6 +369,14 @@ document.getElementById("taskName").addEventListener("keyup", function () {
   }
 });
 
+// let activeTasks = document.getElementsByClassName("postedTask");
+
+// for (var i = 0; i < activeTasks.length; i++) {
+//   activeTasks[i].parentElement.addEventListener("click", function (data) {
+//     showUpdateTasker(data);
+//   });
+// }
+
 initialAppSettings();
-retrieveActiveTasks();
-retrieveCompletedTasks();
+pullTasksFromLocalStorage();
+displasyCompletedTasks();
